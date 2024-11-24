5b21a3b2e6c254b27fc185550e03de9ce6511067
dionatanalves <dionatan03_alves@discente.ufg.br>
2024-11-24 17:26:27 -0600

    Contract update

--- blockchain/contracts/Croakmarket.sol
+++ blockchain/contracts/Croakmarket.sol
@@ -1,160 +1,98 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;  // Atualizada para versão 0.8.27
-
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+pragma solidity ^0.8.19;
 
 /// @title Croakmarket
-/// @notice A secure marketplace contract for minting, listing, purchasing, auctioning, and transferring items.
-contract Croakmarket is ReentrancyGuard {
-
-    /// @notice Fee percentage taken by the marketplace (2%).
-    uint public constant FEE_PERCENTAGE = 2;
-
-    /// @notice Owner of the contract.
-    address public owner;
-
-    /// @notice Structure to represent an item in the marketplace.
+/// @notice A simple marketplace contract for listing, purchasing, and transferring items
+/// @dev This contract manages items, their ownership, and transactions
+contract Croakmarket {
+    /// @notice Structure to represent an item in the marketplace
+    /// @dev Each item has a unique ID, name, price, seller, owner, and sale status
     struct Item {
         uint id;
         string name;
-        string description;
         uint price;
         address payable seller;
         address owner;
         bool isSold;
     }
 
-    /// @notice Structure to represent an auction in the marketplace.
-    struct Auction {
-        uint id;
-        uint itemId;
-        uint startPrice;
-        uint currentBid;
-        address currentBidder;
-        uint endTime;
-        bool isActive;
-    }
-
+    /// @notice Total number of items listed in the marketplace
     uint public itemCount = 0;
-    uint public auctionCount = 0;
 
+    /// @notice Mapping of item IDs to Item structs
     mapping(uint => Item) public items;
-    mapping(uint => Auction) public auctions;
 
-    event ItemMinted(uint indexed itemId, string name, string description, uint price, address indexed seller);
-    event ItemPurchased(uint indexed itemId, address indexed buyer, uint price, uint fee);
-    event AuctionCreated(uint indexed auctionId, uint itemId, uint startPrice, uint endTime);
-    event NewBid(uint indexed auctionId, address indexed bidder, uint bidAmount);
-    event AuctionEnded(uint indexed auctionId, uint itemId, address indexed winner, uint finalBid);
+    /// @notice Mapping of owner addresses to arrays of owned item IDs
+    mapping(address => uint[]) public ownedItems;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can perform this action");
-        _;
-    }
-
-    modifier auctionIsActive(uint auctionId) {
-        require(auctions[auctionId].isActive, "Auction is not active");
-        require(block.timestamp <= auctions[auctionId].endTime, "Auction has ended");
-        _;
-    }
-
-    constructor() {
-        owner = msg.sender; // Set the deployer as the owner
-    }
-
-    /// @notice Mints a new item in the marketplace.
-    /// @param _name Name of the item.
-    /// @param _description Description of the item.
-    /// @param _price Price of the item in wei.
-    function mintItem(string memory _name, string memory _description, uint _price) external {
+    /// @notice Lists a new item in the marketplace
+    /// @param _name The name of the item
+    /// @param _price The price of the item in wei
+    /// @dev Increments itemCount and adds the new item to the items mapping
+    function listItem(string memory _name, uint _price) public {
         require(_price > 0, "Price must be greater than zero");
-        require(bytes(_name).length > 0, "Name cannot be empty");
-        require(bytes(_description).length > 0, "Description cannot be empty");
 
         itemCount++;
-        items[itemCount] = Item(itemCount, _name, _description, _price, payable(msg.sender), msg.sender, false);
-        emit ItemMinted(itemCount, _name, _description, _price, msg.sender);
+        items[itemCount] = Item(itemCount, _name, _price, payable(msg.sender), msg.sender, false);
+        ownedItems[msg.sender].push(itemCount);
     }
 
-    /// @notice Purchases an item from the marketplace.
-    /// @param _itemId ID of the item to purchase.
-    function purchaseItem(uint _itemId) external payable nonReentrant {
-        Item storage item = items[_itemId];
-        require(item.id == _itemId, "Item does not exist");
-        require(!item.isSold, "Item has already been sold");
-        require(msg.value >= item.price, "Insufficient payment");
+    /// @notice Allows a user to purchase an item
+    /// @param _id The ID of the item to purchase
+    /// @dev Transfers the item's price to the seller and updates ownership
+    function purchaseItem(uint _id) public payable {
+        Item storage item = items[_id];
+        require(_id > 0 && _id <= itemCount, "Item does not exist");
+        require(msg.value == item.price, "Incorrect price");
+        require(!item.isSold, "Item already sold");
+        require(msg.sender != item.seller, "Seller cannot buy their own item");
 
-        uint fee = (item.price * FEE_PERCENTAGE) / 100;
-        uint sellerAmount = item.price - fee;
-
-        item.seller.transfer(sellerAmount);
-        payable(owner).transfer(fee);
-        item.owner = msg.sender;
         item.isSold = true;
+        item.seller.transfer(msg.value);
 
-        emit ItemPurchased(_itemId, msg.sender, item.price, fee);
+        // Transfer ownership
+        _transferOwnership(_id, item.seller, msg.sender);
     }
 
-    /// @notice Creates a new auction for an item.
-    /// @param _itemId ID of the item to be auctioned.
-    /// @param _startPrice Starting price of the auction in wei.
-    /// @param _duration Duration of the auction in seconds.
-    function createAuction(uint _itemId, uint _startPrice, uint _duration) external {
-        Item storage item = items[_itemId];
-        require(item.id == _itemId, "Item does not exist");
-        require(!item.isSold, "Item has already been sold");
-        require(item.owner == msg.sender, "Only the owner can auction this item");
-        require(_duration > 0, "Auction duration must be greater than zero");
+    /// @notice Internal function to transfer ownership of an item
+    /// @param _id The ID of the item
+    /// @param _from The current owner's address
+    /// @param _to The new owner's address
+    /// @dev Updates the item's owner and adjusts the ownedItems mappings
+    function _transferOwnership(uint _id, address _from, address _to) internal {
+        Item storage item = items[_id];
+        item.owner = _to;
 
-        auctionCount++;
-        uint endTime = block.timestamp + _duration;
-        auctions[auctionCount] = Auction(
-            auctionCount,
-            _itemId,
-            _startPrice,
-            0,
-            address(0),
-            endTime,
-            true
-        );
-        emit AuctionCreated(auctionCount, _itemId, _startPrice, endTime);
-    }
-
-    /// @notice Places a bid on an active auction.
-    /// @param _auctionId ID of the auction to bid on.
-    function placeBid(uint _auctionId) external payable auctionIsActive(_auctionId) nonReentrant {
-        Auction storage auction = auctions[_auctionId];
-        require(msg.value > auction.currentBid, "Bid must be higher than the current bid");
-
-        if (auction.currentBidder != address(0)) {
-            payable(auction.currentBidder).transfer(auction.currentBid);
+        // Remove item from the previous owner's list
+        uint[] storage fromItems = ownedItems[_from];
+        for (uint i = 0; i < fromItems.length; i++) {
+            if (fromItems[i] == _id) {
+                fromItems[i] = fromItems[fromItems.length - 1];
+                fromItems.pop();
+                break;
+            }
         }
 
-        auction.currentBid = msg.value;
-        auction.currentBidder = msg.sender;
-        emit NewBid(_auctionId, msg.sender, msg.value);
+        // Add item to the new owner's list
+        ownedItems[_to].push(_id);
     }
 
-    /// @notice Ends an auction and transfers the item to the highest bidder.
-    /// @param _auctionId ID of the auction to end.
-    function endAuction(uint _auctionId) external nonReentrant {
-        Auction storage auction = auctions[_auctionId];
-        require(auction.isActive, "Auction is not active");
-        require(block.timestamp > auction.endTime, "Auction is still ongoing");
+    /// @notice Allows the owner to transfer an item to another address
+    /// @param _id The ID of the item to transfer
+    /// @param _to The address of the recipient
+    /// @dev Calls the internal _transferOwnership function
+    function transferItem(uint _id, address _to) public {
+        Item storage item = items[_id];
+        require(_id > 0 && _id <= itemCount, "Item does not exist");
+        require(msg.sender == item.owner, "You do not own this item");
 
-        auction.isActive = false;
-        Item storage item = items[auction.itemId];
+        _transferOwnership(_id, msg.sender, _to);
+    }
 
-        if (auction.currentBidder != address(0)) {
-            uint fee = (auction.currentBid * FEE_PERCENTAGE) / 100;
-            uint sellerAmount = auction.currentBid - fee;
-
-            item.owner = auction.currentBidder;
-            item.isSold = true;
-            item.seller.transfer(sellerAmount);
-            payable(owner).transfer(fee);
-
-            emit AuctionEnded(_auctionId, auction.itemId, auction.currentBidder, auction.currentBid);
-        }
+    /// @notice Retrieves all item IDs owned by a specific address
+    /// @param _owner The address of the owner
+    /// @return An array of item IDs owned by the specified address
+    function getItemsByOwner(address _owner) public view returns (uint[] memory) {
+        return ownedItems[_owner];
     }
 }
